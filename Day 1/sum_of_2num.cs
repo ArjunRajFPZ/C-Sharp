bcf697ed5e5214f1b031f62a459ae2e2dfe6db17
ARJUN RAJ R S <95402319+ArjunRajFPZ@users.noreply.github.com>
2023-07-07 12:35:09 +0530

    Create sum_of_2num.cs

---
+++ Day 1/sum_of_2num.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Program1
+{
+  internal class Program2
+  {
+    static void Main(string[] args)
+    {
+      int num1 = 10,num2 = 20;
+      Console.WriteLine(num1 + num2);
+    }
+  }
+}
