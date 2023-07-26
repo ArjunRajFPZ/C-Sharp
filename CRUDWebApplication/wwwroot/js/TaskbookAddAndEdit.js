346f2219f898895070c92ba122fa650d9de7e2d5
ArjunRajFPZ <arjunrajcbse@gmail.com>
2023-07-26 19:55:47 +0530

    jQuery Validation Done

---
+++ CRUDWebApplication/wwwroot/js/TaskbookAddAndEdit.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    $("#taskbookform").on("submit", function (e) {
+        var inputName = $("#name").val();
+        var inputAssignedfrom = $("#assignedfrom").val();
+        var inputAssignedto = $("#assignedto").val();
+        if (inputName.match(/^([a-zA-Z ]{3,})$/)) {
+            if (inputAssignedfrom.match(/^([a-zA-Z ]{3,})$/)) {
+                if (inputAssignedto.match(/^([a-zA-Z ]{3,})$/)) {
+                    return;
+                } else {
+                    e.preventDefault();
+                    alert("Entered assigned to contains number or whitespace! Please enter only characters");
+                    return;
+                }
+            } else {
+                e.preventDefault();
+                alert("Entered assigned from contains number or whitespace! Please enter only characters");
+                return;
+            }
+        } else {
+            e.preventDefault();
+            alert("Entered username contains number or whitespace! Please enter only characters");
+            return;
+        }
+    });
+});
---
+++ CRUDWebApplication/wwwroot/js/userAddAndEditValidation.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    $("#userform").on("submit", function (e) {
+        var inputName = $("#name").val();
+        var inputaddress = $("#address").val();
+        var inputemail = $("#inputemail").val();
+        if (inputName.match(/^([a-zA-Z ]{3,})$/)) {
+            if (inputaddress.match(/^([a-zA-Z0-9., ]{3,})$/)) {
+                if (inputemail.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+                    return;
+                } else {
+                    e.preventDefault();
+                    alert("Entered email is not correct!");
+                    return;
+                }
+            } else {
+                e.preventDefault();
+                alert("Entered address contains only whitespace or special Characters!");
+                return;
+            }
+        } else {
+            e.preventDefault();
+            alert("Entered username contains number or whitespace! Please enter only characters");
+            return;
+        }
+    });
+});
