a721f6de6dac2add0e365d266d8e44da7900fb6d
ArjunRajFPZ <arjunrajcbse@gmail.com>
2023-07-28 18:27:40 +0530

    Identity Done

--- CRUDWebApplication/Data/DatabaseContext.cs
+++ CRUDWebApplication/Data/DatabaseContext.cs
@@ -1,9 +1,10 @@
 ﻿using CRUDWebApplication.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CRUDWebApplication.Data
 {
-    public class DatabaseContext : DbContext
+    public class DatabaseContext : IdentityDbContext
     {
         public DatabaseContext(DbContextOptions options) : base(options)
         {
--- CRUDWebApplication/Program.cs
+++ CRUDWebApplication/Program.cs
@@ -1,5 +1,6 @@
 using CRUDWebApplication.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AppConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<DatabaseContext>();
 
 
 var app = builder.Build();
@@ -23,11 +27,14 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
+app.MapRazorPages();
+
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Signin}/{id?}");
 
 app.Run();
